2fa84e1d939558b8412301a581e247d209f0ceef
Daniela Barbosa <contato@danielacb.com>
2021-07-22 21:08:59 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5a78025d5296a5fd1207e7c21126b400'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd9f4b50a-5021-4406-8052-7ceaf18ad7e4'
+};
