b4d1e8932ebd32ae3e09cab98da6d91497e60f53
Daniela Barbosa <contato@danielacb.com>
2021-08-14 21:27:06 -0300

    add netlify webhook

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -1,8 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+const axios = require("axios");
+const netlifyWebhook = strapi.config.get("custom.netlifyWebhook");
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(_, __) {
+      netlifyWebhook &&
+        axios.post(
+          "https://api.netlify.com/build_hooks/61185b10fc2c0b6a946c6561"
+        );
+    },
+    async afterUpdate(_, __, ___) {
+      netlifyWebhook &&
+        axios.post(
+          "https://api.netlify.com/build_hooks/61185b10fc2c0b6a946c6561"
+        );
+    },
+  },
+};
